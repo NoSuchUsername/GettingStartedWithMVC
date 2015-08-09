392888b2d5895eee98854ce92f66118795154117
NoSuchUsername <gevorg.oksuzyan@gmail.com>
2015-08-09 19:16:42 +0400

    Added view and controller to the project. Next to tutorial 4.

--- MVC1/Controllers/HelloWorldController.cs
+++ MVC1/Controllers/HelloWorldController.cs
@@ -36,9 +36,17 @@ namespace MVC1.Controllers
         //    return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
         //}
 
-        public string Welcome(string name, int ID = 1)
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        //}
+
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
         }
     }
 }
